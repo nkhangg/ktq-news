ea746e8a85a81946ff1349d10cc83404557036b3
nkhangg <phamnhutkhanggpc04910@gmail.com>
2025-02-28 13:30:29 +0700

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
+const siteURL = process.env.NEXT_PUBLIC_SITE_URL || 'https://ktq-blogs.com';
+
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://ktq-blogs.com',
+    siteUrl: siteURL,
     generateRobotsTxt: true,
     sitemapSize: 5000,
     changefreq: 'daily',
@@ -13,6 +15,45 @@ const config = {
             priority: path === '/' ? 1.0 : 0.8,
             lastmod: new Date().toISOString(),
         };
+    },
+    additionalPaths: async () => {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/posts/sitemaps`, { next: { revalidate: 1800 } });
+
+        const result = await response.json();
+
+        const data = result?.data || [];
+
+        const dynamicSitemaps = data.map((item) => {
+            return {
+                loc: `${siteURL}/${item.category.slug}/${item.slug}`,
+                lastmod: new Date(item.updated_at),
+                changefreq: 'daily',
+                priority: 0.8,
+            };
+        });
+
+        const static = [
+            {
+                loc: `${siteURL}/posts`,
+                lastmod: new Date(),
+                changefreq: 'daily',
+                priority: 0.7,
+            },
+            {
+                loc: `${siteURL}/contact`,
+                lastmod: new Date(),
+                changefreq: 'monthly',
+                priority: 0.5,
+            },
+            {
+                loc: `${siteURL}/about`,
+                lastmod: new Date(),
+                changefreq: 'monthly',
+                priority: 0.5,
+            },
+        ];
+
+        return [...dynamicSitemaps, ...static];
     },
 };
 
